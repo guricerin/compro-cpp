acd401bad736bf92e098a48bcc6496cc7737c491
guricerin <chanbo1e9@gmail.com>
2021-09-05 16:59:16 +0900

    add square_nums

--- library/math/divisor.cpp
+++ library/math/divisor.cpp
@@ -15,3 +15,20 @@ vector<i64> divisors(i64 n) {
     sort(res.begin(), res.end());
     return res;
 }
+
+/// nは平方数であるか
+/// 平方数: 約数が奇数個の数値
+bool is_square(i64 n) {
+    // √n を整数にキャストする
+    // 数値誤差を回避するため、EPS = 10^-9 を足している
+    const double eps = 1e-9;
+    i64 sq = (i64)(sqrt(n) + eps);
+    return (sq * sq) == n;
+}
+
+/// n以下の平方数の個数
+i64 square_nums(i64 n) {
+    const double eps = 1e-9;
+    i64 sq = (i64)(sqrt(n) + eps);
+    return sq;
+}
