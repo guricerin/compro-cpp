cfea55a9ea0fee73ac36bf5668f7d8fef0b634ce
guricerin <chanbo1e9@gmail.com>
2021-10-26 16:31:32 +0900

    unionfind: https://algo-method.com/descriptions/136

--- library/data-structure/union_find.cpp
+++ library/data-structure/union_find.cpp
@@ -2,35 +2,37 @@
 
 /// 素集合データ構造: Union Find Tree (Disjoint Set)
 struct UnionFind {
-    vector<int> _ids; // 添字iが属する集合のID
+    vector<int> _par, _rank, _siz;
 
-    UnionFind(int n) {
-        _ids.assign(n, -1);
-    }
+    UnionFind(int n) : _par(n, -1), _rank(n, 0), _siz(n, -1) {}
 
+    // 根を求める
     int root(int x) {
-        const auto rx = _ids[x];
-        return rx < 0 ? x : _ids[x] = root(rx);
+        if (_par[x] == -1)
+            return x; // x が根の場合は x を返す
+        else
+            return _par[x] = root(_par[x]); // 経路圧縮
     }
 
-    // 要素xとyは同じ連結成分に属するか？
+    // x と y が同じグループに属するか (= 根が一致するか)
     bool connected(int x, int y) {
         return root(x) == root(y);
     }
 
-    // 要素xが属する集合の要素数
-    int size(int x) {
-        return -_ids[root(x)];
+    // x を含むグループと y を含むグループを併合する
+    bool unite(int x, int y) {
+        int rx = root(x), ry = root(y); // x 側と y 側の根を取得する
+        if (rx == ry) return false;     // すでに同じグループのときは何もしない
+        // union by _rank
+        if (_siz[rx] < _siz[ry]) swap(rx, ry);   // ry 側の _rank が小さくなるようにする
+        _par[ry] = rx;                           // ry を rx の子とする
+        if (_rank[rx] == _rank[ry]) _rank[rx]++; // rx 側の _rank を調整する
+        _siz[rx] += _siz[ry];                    // rx 側の _siz を調整する
+        return true;
     }
 
-    // 要素xとyを連結
-    bool unite(int x, int y) {
-        auto rx = root(x), ry = root(y);
-        if (rx == ry) return false;
-
-        if (_ids[rx] > _ids[ry]) swap(rx, ry);
-        _ids[rx] += _ids[ry];
-        _ids[ry] = rx;
-        return true;
+    // x を含む根付き木のサイズを求める
+    int size(int x) {
+        return _siz[root(x)];
     }
 };
