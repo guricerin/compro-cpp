2693c7780ac29de220b21d32fa1afee0f885032c
guricerin <chanbo1e9@gmail.com>
2022-01-27 11:47:27 +0900

    add statistics

---
+++ library/math/statistics.cpp
@@ -0,0 +1,28 @@
+#include "../common.hpp"
+
+/// 平均
+template <class T>
+double average(const vector<T>& ls) {
+    return accumulate(all(ls), 0.0) / (double)ls.size();
+}
+
+/// 分散 : データの散らばり具合
+/// 各要素に対する「データの平均値からの距離の二乗」の平均値
+/// 分散が0 -> データの値がすべて等しい
+template <class T>
+double variance(const vector<T>& ls) {
+    auto ave = average(ls);
+    double sum = 0;
+    for (auto l : ls) {
+        sum += pow(l - ave, 2);
+    }
+    return sum / (double)ls.size();
+}
+
+/// 標準偏差 : 分散の非負の平方根
+/// 分散と比較した場合、各データの単位と一致するというメリットがある
+template <class T>
+double std_deviation(const vector<T>& ls) {
+    auto omega = variance(ls);
+    return sqrt(omega);
+}
