f8138521a8488e8c1d63b5d55da12f0d8b5b9c18
guricerin <chanbo1e9@gmail.com>
2022-01-27 18:04:58 +0900

    fix

--- library/math/statistics.cpp
+++ library/math/statistics.cpp
@@ -6,17 +6,6 @@ double average(const vector<T>& ls) {
     return accumulate(all(ls), 0.0) / (double)ls.size();
 }
 
-/// 中央値
-template <class T>
-double median(const vector<T>& ls) {
-    int mid = ls.size() / 2;
-    if (ls.size() & 1) {
-        return ls[mid];
-    } else {
-        return (ls[mid - 1] + ls[mid]) / 2.0;
-    }
-}
-
 /// 分散 : データの散らばり具合
 /// 各要素に対する「データの平均値からの距離の二乗」の平均値
 /// 分散が0 -> データの値がすべて等しい
@@ -36,6 +25,17 @@ template <class T>
 double std_deviation(const vector<T>& ls) {
     auto omega = variance(ls);
     return sqrt(omega);
+}
+
+/// 中央値
+template <class T>
+double median(const vector<T>& ls) {
+    int mid = ls.size() / 2;
+    if (ls.size() & 1) {
+        return ls[mid];
+    } else {
+        return (ls[mid - 1] + ls[mid]) / 2.0;
+    }
 }
 
 /// 四分位数
